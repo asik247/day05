2927bf730921d66e29c5f5948ef3bfd2fddd6fb3
Asikur Rahman <mdasikur5893@gmail.com>
2026-01-21 16:17:05 +0600

    commit now

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function sayHello(){
 }
 let result = sayHello()
 console.log(result);
+
+// funk paramite and argunment:
+function paraArgu(a,b){
+    return a*b;
+}
+let res = paraArgu(10,5);
+console.log(res);
+function advance(name){
+    return "Hello " + name;
+}
+let na = advance("asik");
+console.log(na);
